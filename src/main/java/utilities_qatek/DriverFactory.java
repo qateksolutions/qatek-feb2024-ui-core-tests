7d7b1b2d56f0b82620247d31bf07e97121c850ca
Rifat Halim <rhalim@magicleap.com>
2024-06-01 14:17:35 -0400

    Saving the project

---
+++ src/main/java/utilities_qatek/DriverFactory.java
@@ -0,0 +1,72 @@
+package utilities_qatek;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class DriverFactory {
+    // Do not allow to initialize this class from outside
+    private DriverFactory() {
+        // Do nothing -- Empty constructor
+    }
+
+    private static final DriverFactory instance = new DriverFactory();
+    private static final Logger LOGGER = LogManager.getLogger(DriverFactory.class);
+
+    public static DriverFactory getInstance() {
+        return instance;
+    }
+
+    // Thread local driver object for WebDriver
+    ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(() -> {
+        String environment = System.getProperty("environment") == null ? "local" : System.getProperty("environment");
+        String browser = System.getProperty("browser") == null ? "chrome" : System.getProperty("browser");
+        URL gridUrl = null;
+
+        try {
+            gridUrl = new URL(ReadConfigFiles.getPropertyValues("GridUrl"));
+        } catch (Exception e) {
+            LOGGER.error("Grid URL is incorrect: "  + e.getMessage());
+        }
+
+        if (environment.equals("remote") && browser.equals("chrome")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--remote-allow-origins=*");
+            LOGGER.debug("Executing automation scripts in: " + gridUrl + " in: " + browser);
+            return new RemoteWebDriver(gridUrl, chromeOptions);
+        } else if (environment.equals("remote") && browser.equals("firefox")) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            LOGGER.debug("Executing automation scripts in: " + gridUrl + " in: " + browser);
+            return new RemoteWebDriver(gridUrl, firefoxOptions);
+        } else if (environment.equals("remote") && browser.equals("edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            LOGGER.debug("Executing automation scripts in: " + gridUrl + " in: " + browser);
+            return new RemoteWebDriver(gridUrl, edgeOptions);
+        } else {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            LOGGER.debug("Executing automation scripts in: " + environment + " in: " + browser);
+            return new ChromeDriver(options);
+        }
+    });
+
+    // Call this method to get the driver object and launch the browser
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    // Quits the driver and closes the browser
+    public void removeDriver() {
+        getDriver().quit();
+        driver.remove();
+    }
+}
